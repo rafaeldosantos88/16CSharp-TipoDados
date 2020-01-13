97735512a8ad8ad1de078d8d96f62898c8abae35
Rafael Dos Santos <raphael_979@hotmail.com>
2020-01-13 17:21:37 -0300

    Tipos Basicos de Dados

---
+++ TiposBasicosDados/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TiposBasicosDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n1 = 255; //OuverFlow quando um Calculo estrapola o limite da sua variavel.nO CASO É ATÉ 256
+            n1++;
+            
+
+            Console.WriteLine(n1);
+        }
+    }
+}
